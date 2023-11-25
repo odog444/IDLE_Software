02aab31fff684a4c78ec01075043737e114bf625
Rootbryce <brycethai2@gmail.com>
2023-11-24 20:01:19 -0700

    Update

--- digMode.py
+++ digMode.py
@@ -6,3 +6,12 @@ def digMode():
         dig()
     else:
         enterSafeMode()
+
+    return
+
+def dig():
+    diggerMotor.isOn = True
+    while(weightCollected <= 0.5):
+        if(not systemCheck()): # Continuously check if system is running nominally while digging
+            enterSafeMode()
+    diggerMotor.isOn = False
