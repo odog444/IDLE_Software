dd12a0dc6068ecbdbe4ef3977252727736da715e
mada5670 <150070979+mada5670@users.noreply.github.com>
2024-03-04 11:50:42 -0700

    Update motor_delay.py

--- motorCode/motor_delay.py
+++ motorCode/motor_delay.py
@@ -1,6 +1,7 @@
 # converts throttle setting from -1 to 1 to the associated motor signal pulse range 
 # right now it just outputs the microsecond delay and pulse range based on the throttle from -1 to 1
 
+
 throttle = float(input("Input a number from -1 to 1: "))
 
 # full reverse p <= 500 (throttle <= -1)
@@ -13,21 +14,32 @@ min = 500
 max = 2500
 
 delay = (throttle + 1)*(max - min)/2 + 500
-print("delay is %.2f microseconds" %delay)
 
 
-if delay <= min:
+if delay < 0:
+    print("Throttle is outside of range, corresponding microsecond delay is < 0")
+    range = False
+
+elif delay <= min:
     print("range = full reverse")
+    range = True
 
 elif min < delay < 1490:
     print("range = prop. reverse")
+    range = True
 
 elif 1490 <= delay <= 1510:
     print("range = neutral")
+    range = True
 
 elif 1510 < delay < 2500:
     print("range = prop. forward")
+    range = True
 
 else:
     print("range = full forward")
+    range = True
+
+if range == True:
+    print("delay is %.2f microseconds" %delay)
 
