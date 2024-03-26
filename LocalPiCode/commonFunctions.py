8cc48093bb73386ddac5f46da3094e2c0ca636c8
Rootbryce <brycethai2@gmail.com>
2024-03-26 12:59:10 -0600

    changes to gitignore and general updates

--- LocalPiCode/commonFunctions.py
+++ LocalPiCode/commonFunctions.py
@@ -10,12 +10,35 @@ from multiprocessing import Process
 import time
 import numpy as np
 from faker import Faker
+import RPi.GPIO as GPIO
 
 # Local imports from IDLE software
 import sleepMode
 import safeMode
 import digMode
 import safeMode
+
+'''
+NOTES ON PIN USAGE
+Linear Actuator:
+    Arduino:
+        PWM: pin 9
+        pin1: 8
+        Pin2: 7
+        Pin3: 6
+        Read in: 5 from Pi pin 32
+
+    Pi:
+        PWM: pin 32
+
+Drum motor:
+    Arduino:
+        Output drum: pin 3
+        PWM input: 10
+    
+    Pi:
+        PWM GPIO 13: pin 33
+'''
 
 def systemCheck(): # Check if all sensors have nominal readings and 
     if(not checkSensors()):
